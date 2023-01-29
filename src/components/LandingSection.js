086da594f54e5a14c3f230d1033640df9af89648
Hussain ASelmia <hussain87nyn@gmail.com>
2023-01-29 16:23:53 +0200

    portfolio

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,26 +1,37 @@
-import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
-import FullScreenSection from "./FullScreenSection";
-
-const greeting = "Hello, I am Hussain!";
-const bio1 = "A full-stack developer";
-const bio2 = "specialized in Node.js & React.js";
-
-const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-    <VStack spacing={8}>
-      <Avatar size="2xl" name="Hussain" src="../images/Untitled design3.png" />
-      <Heading as="h1" fontSize="2xl">
-        {greeting}
-      </Heading>
-      <Heading as="h3" fontSize="lg">
-        {bio1} {bio2}
-      </Heading>
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+ 
+const greeting = "Hello, I am Hussain!"; 
+const bio1 = "A full-stack web developer"; 
+const bio2 = "specialized in Node.js & React.js"; 
+ 
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar 
+         src="https://i.pravatar.cc/150?img=7" 
+         size="2xl" 
+         name="Your Name" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1}> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio1} 
+       </Heading> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading> 
+     </VStack> 
     </VStack>
   </FullScreenSection>
 );
